92925cde240c747e2f0fa8d37eb846ae6b9b9103
esmvcs <esmvcs@rit.edu>
2026-03-02 12:51:12 -0500

    Mon 3/2 - 12pm virtual details

--- 12pm/Demos/Week 8/Inheritance_pt2/Cleric.h
+++ 12pm/Demos/Week 8/Inheritance_pt2/Cleric.h
@@ -2,16 +2,24 @@
 
 #include "Character.h"
 
+// OVERRIDE == Call this INSTEAD of the parent one!
+// Technically optional, but really good idea since it'll catch method
+// signature mismatches
+// 
+// In older versions of C++ it meant "call INSTEAD" -- which would
+// be a bad idea for destructors
+// https://learn.microsoft.com/en-us/cpp/cpp/override-specifier?view=msvc-170
+
 class Cleric :
 	public Character
 {
 public:
 	Cleric();
 	Cleric(std::string _name, std::string _deity);
-	~Cleric();	
+	~Cleric();	// NEVER OVERRIDE
 
-	void PrintData();
-	void PrintType();
+	void PrintData() /*const*/ override;
+	void PrintType() /*const*/ /* override */;
 
 protected:
 	std::string deity;
